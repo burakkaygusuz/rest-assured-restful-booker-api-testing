e83b787fa30fafe22b64e617d4aa9156f55689f1
Burak Kaygusuz <burakkaygusuz@outlook.com>
2024-12-03 11:22:08 +0300

    Validate object values

--- src/test/java/io/github/burakkaygusuz/objects/Booking.java
+++ src/test/java/io/github/burakkaygusuz/objects/Booking.java
@@ -2,12 +2,16 @@ package io.github.burakkaygusuz.objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonSerialize
-public record Booking(@JsonProperty("firstname") String firstName,
-                      @JsonProperty("lastname") String lastName,
-                      @JsonProperty("totalprice") int totalPrice,
-                      @JsonProperty("depositpaid") boolean depositPaid,
-                      @JsonProperty("bookingdates") BookingDates bookingDates,
-                      @JsonProperty("additionalneeds") String additionalNeeds) {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record Booking(
+    @JsonProperty("firstname") String firstName,
+    @JsonProperty("lastname") String lastName,
+    @JsonProperty("totalprice") @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT) int totalPrice,
+    @JsonProperty("depositpaid") @JsonFormat(shape = JsonFormat.Shape.BOOLEAN) boolean depositPaid,
+    @JsonProperty("bookingdates") BookingDates bookingDates,
+    @JsonProperty("additionalneeds") String additionalNeeds) {
 }
--- src/test/java/io/github/burakkaygusuz/objects/BookingDates.java
+++ src/test/java/io/github/burakkaygusuz/objects/BookingDates.java
@@ -1,9 +1,29 @@
 package io.github.burakkaygusuz.objects;
 
+import java.text.MessageFormat;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @JsonSerialize
-public record BookingDates(@JsonProperty("checkin") String checkIn,
-                           @JsonProperty("checkout") String checkOut) {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record BookingDates(
+    @JsonProperty("checkin") String checkIn,
+    @JsonProperty("checkout") String checkOut) {
+
+  public BookingDates {
+    if (checkIn != null) {
+      validateDate(checkIn, "checkin");
+    }
+    if (checkOut != null) {
+      validateDate(checkOut, "checkout");
+    }
+  }
+
+  private static void validateDate(String date, String fieldName) {
+    if (!date.matches("\\d{4}-\\d{2}-\\d{2}")) {
+      throw new IllegalArgumentException(MessageFormat.format("Invalid {0} date format. Use yyyy-MM-dd", fieldName));
+    }
+  }
 }
