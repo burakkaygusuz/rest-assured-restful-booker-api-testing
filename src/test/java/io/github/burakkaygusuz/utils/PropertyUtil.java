6a282c634693c21a8fe4831520010f651640dd7c
Burak Kaygusuz <burakkaygusuz@outlook.com>
2025-06-25 22:20:13 +0300

    refactor(utils): replace singleton implementation in PropertyUtil

--- src/test/java/io/github/burakkaygusuz/utils/PropertyUtil.java
+++ src/test/java/io/github/burakkaygusuz/utils/PropertyUtil.java
@@ -3,32 +3,31 @@ package io.github.burakkaygusuz.utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PropertyUtil {
 
-    private static volatile PropertyUtil instance = null;
-    private final Properties properties;
+  private static final Map<String, PropertyUtil> INSTANCES = new ConcurrentHashMap<>();
+  private final Properties properties;
 
-    private PropertyUtil(String fileName) {
-        this.properties = new Properties();
-        try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(fileName)) {
-            properties.load(inputStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  public static PropertyUtil getInstance(String fileName) {
+    return INSTANCES.computeIfAbsent(fileName, PropertyUtil::new);
+  }
 
-    public static PropertyUtil getInstance(String fileName) {
-        if (instance == null) {
-            synchronized (PropertyUtil.class) {
-                if (instance == null)
-                    instance = new PropertyUtil(fileName);
-            }
-        }
-        return instance;
+  private PropertyUtil(String fileName) {
+    this.properties = new Properties();
+    try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(fileName)) {
+      if (inputStream == null) {
+        throw new IllegalArgumentException("Property file not found: " + fileName);
+      }
+      properties.load(inputStream);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public String getProperty(String key) {
-        return properties.getProperty(key);
-    }
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
